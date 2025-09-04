087c8ea8a9673ab714b772d6d74447b65ad3cdf8
GHEBRIOUA Rayane <102435036+GhebriouaRayane@users.noreply.github.com>
2025-09-04 02:31:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 // Autoriser uniquement ton frontend Vercel
 app.use(cors({
   origin: 'https://dzloc.vercel.app',
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
@@ -17,7 +17,5 @@ app.post('/auth/register', (req, res) => {
   res.json({ message: 'Inscription réussie !' });
 });
 
-// Lancer le serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Backend lancé sur le port ${PORT}`));
-
